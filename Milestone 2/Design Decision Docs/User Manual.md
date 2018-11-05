1c75d4f163b61b7d8764c4181565f197fb159d8b
Youssef Saghbini <swaghbini@outlook.com>
2018-11-05 11:14:05 -0500

    Origanized
    Added Milestone folders and Milestone 2 organized into MVC format

---
+++ Milestone 2/Design Decision Docs/User Manual.md
@@ -0,0 +1,94 @@
+﻿# Plants VS Zombies User Manual
+*Made by Youssef Saghbini*
+
+Zombies have landed onto your planet and want to take over the whole world. The only way to defend yourself is to use your plants! Supplied with alien-like plants like sunflowers and peashooters, you will need to act fast and plant fast. For you and your plants are the only ones who can save this world!
+
+## Description
+In this version of plants versus zombies, it will be based of turns, but the objective is the same, where you need to eliminate the zombies. A turn-based game is where you have limited amount of actions to do within a round. Each round will consist of one action that the user will be given in the menu. The menu will be given throughout the gameplay. Zombies will be spawned at random from the farthest side of the plants. Zombies will move one square closer to the other side until reached. The user will need to improvise a strategy to defend of the zombies and win the game.
+
+
+# Layout
+How the game will look.
+## Board 
+![](https://lh3.googleusercontent.com/4ivQKWqFhtfpkQrl_abbGxGrbp2Og-GsgbHm5TAPuWZxFBiWe-jfEZnz4StnC9ijbsKSSpkHDVF1Zw "board")
+
+The one-level based plants versus zombie game will consist of 5 by 8 board, where each row and column are numbered respectively. On the bottom of the board, will indicate how much money you have to spend on your plant. In the game, you will pick where your plants will be placed by x and y coordinates. With the coordinates, it will indicate which square you are pointing at and it will place your plant of choice. These are the current plants available, also will include details about zombie;
+
+| Piece | Cost | Attack | Health | Generates Money | Symbol|
+|--|--|--|--|--|--|
+|Sunflower|5|0|5|5|S|
+|Peashooter|10|2|5|0|P|
+|Zombie (Enemy)|0|2|5|0|Z|
+ 
+## Menu
+![](https://lh3.googleusercontent.com/ybxdhRCZsANoTCLalVR8Lzy1Kjape9nTLqkrO8vrzL7Z22QeDUOBZfz_-EY4ziiX0kweVGXVpMhrxw "menu")
+
+As the game starts, you will be greeted with this menu. This menu will be with you until you lose, win or end the game. This menu will help you add plants, skip the round and exit the game at anytime. 
+
+## Log 
+![](https://lh3.googleusercontent.com/reP2awdH4ZiWgFqDRReI7Wknv0e4sOpM9U5Jz7iSJ7LRSwYG0BBIfarsogGpnQCehavHP96byvEIiQ "log")
+
+During the game, any event that occurs; will be recorded and displayed, so that the user will be notified on what is happening on the battlefield. Another word, any in-game activities will be logged and shown in the next round.
+
+# Instructions
+
+How to play the game. Each event will be shown in gif and described for better understanding.
+## Start of the game 
+
+Once the game, you will be asked whether to start the game or press any key to exit out.
+
+![Recordit GIF](http://g.recordit.co/oRQpxfTTVH.gif)
+
+If pressed 1, then it will generate a new game.
+
+![Recordit GIF](http://g.recordit.co/cKFGiO4bXo.gif)
+
+If pressed anything else then the program will be exited and close.
+
+
+## Adding Peashooters or Sunflowers
+
+![Recordit GIF](http://g.recordit.co/pU0pKRBDR2.gif)
+
+During in-game, the board and menu will always be presented to you to see the current state. As the menu is shown, you will be given the option to add a plant, whether its peashooter or sunflower, it is up to the user. Once chosen, it will prompt asking you where you would like to place your new plant. It is asking the user for a two digit value, where the first digit will be the column section and the second digit will be the row section. Another words, the two digits are coordinates pointing to a square where the plant will be added. Also, when adding a new plant is must follow three conditions;
+
+ 1. Must be two characters and two characters only
+ 2. Must be within the range of the board
+ 3. Must have enough money to purchase a new plant
+
+## Skipping the round
+
+![Recordit GIF](http://g.recordit.co/4eYLfOh0yI.gif)
+
+In some cases, the user will have little amount of money to with start or during in-game. When the user is at that state, they are limited to skipping the round or exiting the game. If the user contains sunflowers in the game, then each round skipped money will be generated and the user will be able to add plants onto the board. If no sunflowers were added onto the board, then the user can see then end result of the game when skipping the round or they can exit the game. 
+
+## Attacking the frontlines
+
+![Recordit GIF](http://g.recordit.co/Gpt8YmeQpB.gif)
+
+During the game, you will have zombies attack you, so you must defend yourself. The only way to defend yourself is to spawn peashooters. Peashooters are able to attack back, and have range. Once the zombie is within range of the peashooter, their health will drop dependant on the peashooter's attack. Also, whenever a plant or a zombie is attacking it will be logged. The log will help the user see which plant and or zombie is attacking and how much health is left.
+
+## Multiple Sunflowers
+
+![Recordit GIF](http://g.recordit.co/6E79V5x6Fo.gif)
+
+In-game, the sunflower is able to generate 5 units into the user's money pouch. The more sunflower on the board, the more units the user is receive in their money pouch.
+
+
+## Exiting the game
+
+![Recordit GIF](http://g.recordit.co/PwpD6DxilF.gif)
+In the menu, the last option the user is given is the exit option. Where the user is able to exit the game at each round. Once exited, the program will end.
+
+
+## Winning the game 
+
+![Recordit GIF](http://g.recordit.co/iWyRksqoIY.gif)
+
+Since the objective of the game is preventing the zombies from reaching to your end of the board. Once all zombies have been destroyed then the user has won the game.
+
+
+## Losing the game
+![Recordit GIF](http://g.recordit.co/Lom3A6DoWy.gif)
+
+The objective of the game is to prevent zombies from reaching to your home, column 0. The game will check itself, every round, to see if any zombie has reached to the end. If one zombie has reached to column 0, then the user looses the game.
---
+++ Milestone 2/src/controller/GameBoard.java
@@ -0,0 +1,291 @@
+import java.util.Random;
+
+/**
+ * @author Youssef Saghbini
+ * @version 1.4
+ */
+
+public class GameBoard {
+
+    /**
+     *  Dual-array gameboard to be played on.
+     */
+    private Square board[][];
+
+    /**
+     *  User's money pouch during the game.
+     */
+    private int moneyPouch;
+
+    /**
+     *  The amount of zombies to be spawned within the board.
+     */
+    private int zombieLimit;
+
+    /**
+     *  Logging every event happening during the game.
+     */
+    private String logging;
+
+    /**
+     *  Will generate a brand new board with initial values. Board will consist of
+     *  a dual array of squares, and each square would contain a specific coordinate and
+     *  piece when added and or removed. Logging is to keep track of every event happening.
+     *  Money pouch is the amount of money the player will have. ZombieLimit is the
+     *  amount of zombies allowed to be spawned into the board.
+     */
+    public GameBoard(){
+        board = new Square[8][5];
+        moneyPouch = 500;
+        zombieLimit = 5;
+        logging = "";
+        for (int c=0; c<8; c++)
+            for (int r=0; r<5; r++)
+                board[c][r] = new Square(new Coordinate  (c,r));
+        reset();
+    }
+
+    /** Adding pieces around the generated gameBoard. Will use the addPiece()
+     *  and removePiece() methods, when necessary.
+     *  @param coordinate receiving the coordinate at which the piece will be placed
+     *  @param piece receiving the type of piece to be added at specific coordinate
+     *  @return Whether if it is possible to add the piece within conditions
+     */
+    public boolean add(Coordinate coordinate, Piece piece)
+    {
+        Square srcSquare = this.getSquare(coordinate);
+        if(srcSquare.isOccupied()){
+            logging += "Piece not added due to space occupied.\n";
+            return false;
+        }
+        if(!purchasePiece(piece)){
+            logging += "Piece not added due to lack to money.\n";
+            return false;
+        }
+        srcSquare.addPiece(piece);
+        logging += "Added Piece: " + piece.getName() + " @ Coordinates: " + coordinate.toString() + "\n";
+        return true;
+    }
+
+    /**
+     *  This method is to move a piece from one coordinate to another. It will
+     *  receive the current and new coordinate; in those coordinates, the method
+     *  will retrieve the piece and move them.
+     *  @param src the current coordinate in the piece is currently placed
+     *  @param dest the potential new coordinate where the piece will be move to
+     */
+    public void move(Coordinate src, Coordinate dest)
+    {
+        Square srcSquare = this.getSquare(src);
+        Square destSquare = this.getSquare(dest);
+        if (destSquare.isOccupied()){
+            return;
+        }
+        Piece p = srcSquare.getPiece();
+        destSquare.addPiece(p);
+        srcSquare.deletePiece();
+        logging += "Moved Zombie from " + src.toString() + " to " + dest.toString() + "\n";
+        return;
+    }
+
+    /**
+     *  When piece is within range of attack, it will affect the other piece's health.
+     */
+    public void hitUpdate(){
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (board[col][row].getPiece() != null) {
+                    if (board[col][row].getPiece().getHealth() > 0){
+                        if (board[col][row].getPiece().getShortName() == 'P') {
+                            if (board[col + 1][row].getPiece() != null && board[col + 1][row].getPiece().getShortName() == 'Z') {
+                                int x = board[col + 1][row].getPiece().getHealth();
+                                x -= board[col][row].getPiece().getAttack();
+                                board[col + 1][row].getPiece().setHealth(x);
+                                if(board[col + 1][row].getPiece().getHealth() <=0 ) {
+                                    logging += board[col][row].getPiece().getName() + " Health: " + board[col][row].getPiece().getHealth() + " @ " + board[col][row].getCoordinate() + " Attacked " + board[col + 1][row].getPiece().getName() + " Health: Dead @ " + board[col + 1][row].getCoordinate() + "\n";
+                                } else {
+                                    logging += board[col][row].getPiece().getName() + " Health: " + board[col][row].getPiece().getHealth() + " @ " + board[col][row].getCoordinate() + " Attacked " + board[col + 1][row].getPiece().getName() + " Health: " + board[col + 1][row].getPiece().getHealth() + " @ " + board[col + 1][row].getCoordinate() + "\n";
+                                }
+                            } else if (board[col + 2][row].getPiece() != null && board[col + 2][row].getPiece().getShortName() == 'Z') {
+                                int x = board[col + 2][row].getPiece().getHealth();
+                                x -= board[col][row].getPiece().getAttack();
+                                board[col + 2][row].getPiece().setHealth(x);
+                                if(board[col + 2][row].getPiece().getHealth() <=0 ) {
+                                    logging += board[col][row].getPiece().getName() + " Health: " + board[col][row].getPiece().getHealth() + " @ " + board[col][row].getCoordinate() + " Attacked " + board[col + 1][row].getPiece().getName() + " Health: Dead @ " + board[col + 2][row].getCoordinate() + "\n";
+                                } else {
+                                    logging += board[col][row].getPiece().getName() + " Health: " + board[col][row].getPiece().getHealth() + " @ " + board[col][row].getCoordinate() + " Attacked " + board[col + 2][row].getPiece().getName() + " Health: " + board[col + 2][row].getPiece().getHealth() + " @ " + board[col + 2][row].getCoordinate() + "\n";
+                                }
+                            }
+                        }
+                        if (board[col][row].getPiece().getShortName() == 'Z') {
+                            if (!(col - 1 == -1))
+                                if (board[col - 1][row].getPiece() != null && board[col - 1][row].getPiece().getShortName() != 'Z') {
+                                    int x = board[col - 1][row].getPiece().getHealth();
+                                    x -= board[col][row].getPiece().getAttack();
+                                    board[col - 1][row].getPiece().setHealth(x);
+                                    if(board[col - 1][row].getPiece().getHealth() <=0 ) {
+                                        logging += board[col][row].getPiece().getName() + " Health: " + board[col][row].getPiece().getHealth() + " @ " + board[col][row].getCoordinate() + " Attacked " + board[col + 1][row].getPiece().getName() + " Health: Dead @ " + board[col - 1][row].getCoordinate() + "\n";
+                                    } else {
+                                        logging += board[col][row].getPiece().getName() + " Health: " + board[col][row].getPiece().getHealth() + " @ " + board[col][row].getCoordinate() + " Attacked " + board[col - 1][row].getPiece().getName() + " Health: " + board[col - 1][row].getPiece().getHealth() + " @ " + board[col - 1][row].getCoordinate() + "\n";
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     *  Adding zombies randomly at the end of the board.
+     */
+    public void addingZombie(){
+        if (zombieLimit != 0) {
+            Random ran = new Random();
+            int x = ran.nextInt(5);
+            add(new Coordinate(7, x), new Piece(PlantPieces.ZOMBIES));
+            zombieLimit--;
+        }
+    }
+
+    /**
+     *  Used for the zombies to move one square forward after every round.
+     */
+    public void movingZombie(){
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (board[col][row].getPiece() != null) {
+                    if (board[col][row].getPiece().getShortName() == 'Z') {
+                        move(new Coordinate(col, row), new Coordinate(col-1, row));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     *  It will remove pieces when health is equal to zero and below.
+     */
+    public void removeUpdate(){
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (board[col][row].getPiece() != null) {
+                    if (board[col][row].getPiece().getHealth() <= 0)
+                        board[col][row].deletePiece();
+                }
+            }
+        }
+    }
+
+    /**
+     *  Whenever there is a sunflower spawned in the game, it will
+     *  add money into the user's money pouch.
+     */
+    public void sunflowerMoney(){
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 8; col++) {
+                if (board[col][row].getPiece() != null) {
+                    if (board[col][row].getPiece().getShortName() == 'S') {
+                        moneyPouch += 5;
+                        logging += "Sunflower Piece added 5 to your pouch @ Coordinates: " + board[col][row].getCoordinate() + "\n";
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     *  To see if the user is able to purchase a new piece.
+     *  @param piece The piece wanting to purchase.
+     *  @return The ability to purchase a piece.
+     */
+    public boolean purchasePiece(Piece piece){
+        if(moneyPouch < piece.getCost())
+            return false;
+        moneyPouch -= piece.getCost();
+        return true;
+    }
+
+    /**
+     *  Will re-initialize the gameboard, where no piece has spawned.
+     */
+    public void reset(){
+        //Creating two arrays to align with a 2 dimension array
+        board = new Square[8][5];
+        for (int rowsBoard = 0; rowsBoard < 5; rowsBoard++)
+            for (int columnsBoard = 0; columnsBoard < 8; columnsBoard++)
+                board[columnsBoard][rowsBoard] = new Square( new Coordinate(columnsBoard, rowsBoard));
+    }
+
+    /**
+     *  Once all the zombies have been spawned, it will go through all the squares
+     *  in the board; To see if any zombies are "alive". If there are zombies still
+     *  alive then the game keeps going. If all are killed, then the game ends.
+     */
+    public void gameWon(){
+        if(zombieLimit == 0){
+            for (int row = 0; row < 5; row++) {
+                for (int col = 0; col < 8; col++) {
+                    if (board[col][row].getPiece() != null) {
+                        if (board[col][row].getPiece().getShortName() == 'Z') {
+                            return;
+                        }
+                    }
+                }
+            }
+            System.out.println(this.toString() + "\nYou have won the game! Thank you for playing.");
+            System.exit(0);
+        }
+    }
+
+    /**
+     *  Will end the game, if any zombies have reached at the end of the gameboard.
+     */
+    public void gameOver(){
+        for (int row = 0; row < 5; row++) {
+            if (board[0][row].getPiece() != null) {
+                if (board[0][row].getPiece().getShortName() == 'Z') {
+                    System.out.println(this.toString() + "\nYou have lost. Thank you for playing.");
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    /**
+     *  Receiving the square at specific coordinate, as the square contains both the coordinate and piece.
+     *  @param c Coordinate of the square needed
+     *  @return The square at specific coordinate
+     */
+    private Square getSquare (Coordinate c){
+        return board[c.getColumnNumber()][c.getRowNumber()];
+    }
+
+    /**
+     *  @return String implementation of the gameboard. Also, containing logs and money pouch.
+     */
+    public String toString() {
+
+        //Printing in toString() format
+        String s = "\n" + logging + "\n";
+        Piece shortNamePiece;
+        s += "   0 1 2 3 4 5 6 7\n";
+        for (int row = 0; row < 5; row++) {
+            s += row +" |";
+            for (int col = 0; col < 8; col++) {
+                if (board[col][row].getPiece() != null) {
+                    shortNamePiece = board[col][row].getPiece();
+                    s += shortNamePiece.getShortName() + "|";
+                } else {
+                    s += " |";
+                }
+            }
+            s += "\n";
+        }
+        s += "Money Pouch: " + moneyPouch + "\n";
+       // s += logging;
+        logging = "";
+        return s;
+    }
+
+}
