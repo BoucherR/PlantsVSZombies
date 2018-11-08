78d493aae786dbcd0ca56f6a73ac6c9c6cdad702
godrowr <35468510+godrowr@users.noreply.github.com>
2018-11-08 17:50:39 -0500

    Added Peashooter.java

---
+++ Milestone 2/src/model/Peashooter.java
@@ -0,0 +1,22 @@
+package Model;
+
+/**
+ * The Peashooter class provides the PlantsVSZombie game with a single Piece type that
+ * once called can create a Peashooter Piece in of itself. This class only contains a
+ * constructor and simplifies the Piece class code. It uses its super class Piece to 
+ * create a new object and passes it's designated parameters. 
+ * 
+ * @author RG
+ * @version 2.0
+ */
+public class Peashooter extends Piece {
+
+	/**
+	 * Creates a Peashooter with 5 health, 2 attack damage and 20 cost value.
+	 */
+   public Peashooter() {
+        super("PEASHOOTER",'P',5,2,20);
+   }
+
+
+}
