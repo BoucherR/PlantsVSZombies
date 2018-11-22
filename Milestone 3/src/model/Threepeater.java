29bb5d4b003ac9b87af238153718fd001cc886d5
godrowr <35468510+godrowr@users.noreply.github.com>
2018-11-21 19:27:00 -0500

    Add files via upload

---
+++ Milestone 3/src/model/Threepeater.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Threepeater extends Piece {
+
+	public Threepeater() {
+		super("THREEPEATER", 'T', 5, 2, 60);
+	}
+
+}
---
+++ Milestone 3/src/model/TwinSunflower.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class TwinSunflower extends Piece {
+
+	public TwinSunflower() {
+		super("TWINSUNFLOWER", '2', 10, 0, 30);
+	}
+
+}
