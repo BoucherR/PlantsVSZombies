2dd3fe4ce67d0aaece3d0b58527b12666fca7efb
Youssef Saghbini <swaghbini@outlook.com>
2018-11-21 20:32:10 -0500

    Fixed hitUpdate()

--- Milestone 3/src/controller/Controller.java
+++ Milestone 3/src/controller/Controller.java
@@ -299,19 +299,15 @@ public class Controller {
                 if (board[col][row].getPiece() != null) {
                     if (board[col][row].getPiece().getHealth() > 0){
                         if (board[col][row].getPiece().getShortName() == 'P' || board[col][row].getPiece().getShortName() == 'T' || board[col][row].getPiece().getShortName() == 'R' ) {
-                            if (board[col + 1][row].getPiece() != null && board[col + 1][row].getPiece().getShortName() == 'Z') {
-                                board[col + 1][row].getPiece().setHealth(board[col + 1][row].getPiece().getHealth() - board[col][row].getPiece().getAttack());
-                                if(board[col + 1][row].getPiece().getHealth() <=0 ) {
-                                    loggingList.add( board[col][row].getPiece().getName() + " Health: " + board[col][row].getPiece().getHealth() + " @ " + board[col][row].getCoordinate() + " Attacked " + board[col + 1][row].getPiece().getName() + " Health: Dead @ " + board[col + 1][row].getCoordinate() + "\n");
-                                } else {
-                                    loggingList.add( board[col][row].getPiece().getName() + " Health: " + board[col][row].getPiece().getHealth() + " @ " + board[col][row].getCoordinate() + " Attacked " + board[col + 1][row].getPiece().getName() + " Health: " + board[col + 1][row].getPiece().getHealth() + " @ " + board[col + 1][row].getCoordinate() + "\n");
-                                }
-                            } else if (board[col + 2][row].getPiece() != null && board[col + 2][row].getPiece().getShortName() == 'Z') {
-                                board[col + 2][row].getPiece().setHealth(board[col + 2][row].getPiece().getHealth() - board[col][row].getPiece().getAttack());
-                                if(board[col + 2][row].getPiece().getHealth() <=0 ) {
-                                    loggingList.add( board[col][row].getPiece().getName() + " Health: " + board[col][row].getPiece().getHealth() + " @ " + board[col][row].getCoordinate() + " Attacked " + board[col + 2][row].getPiece().getName() + " Health: Dead @ " + board[col + 2][row].getCoordinate() + "\n");
-                                } else {
-                                    loggingList.add( board[col][row].getPiece().getName() + " Health: " + board[col][row].getPiece().getHealth() + " @ " + board[col][row].getCoordinate() + " Attacked " + board[col + 2][row].getPiece().getName() + " Health: " + board[col + 2][row].getPiece().getHealth() + " @ " + board[col + 2][row].getCoordinate() + "\n");
+                            for(int i = 0; i < 8; i++){
+                                if (board[i][row].getPiece() != null)
+                                    if( board[i][row].getPiece().getShortName() == 'Z' || board[i][row].getPiece().getShortName() == 'B' || board[i][row].getPiece().getShortName() == 'C') {
+                                    board[i][row].getPiece().setHealth(board[i][row].getPiece().getHealth() - board[i][row].getPiece().getAttack());
+                                    if (board[i][row].getPiece().getHealth() <= 0) {
+                                        loggingList.add(board[i][row].getPiece().getName() + " Health: " + board[i][row].getPiece().getHealth() + " @ " + board[i][row].getCoordinate() + " Attacked " + board[i][row].getPiece().getName() + " Health: Dead @ " + board[i][row].getCoordinate() + "\n");
+                                    } else {
+                                        loggingList.add(board[i][row].getPiece().getName() + " Health: " + board[i][row].getPiece().getHealth() + " @ " + board[i][row].getCoordinate() + " Attacked " + board[i][row].getPiece().getName() + " Health: " + board[i][row].getPiece().getHealth() + " @ " + board[i][row].getCoordinate() + "\n");
+                                    }
                                 }
                             }
                         }
