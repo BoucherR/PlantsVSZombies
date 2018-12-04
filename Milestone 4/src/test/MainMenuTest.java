32812bdfd996d483beca8fb84770dc0a37188e93
godrowr <35468510+godrowr@users.noreply.github.com>
2018-12-04 12:44:23 -0500

    Added main menu test cases.

--- Milestone 4/src/test/MainMenuTest.java
+++ Milestone 4/src/test/MainMenuTest.java
@@ -4,8 +4,10 @@ import view.MainMenu;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.Before;
-import static org.junit.Assert.*;
 
+import javax.swing.*;
+
+import static org.junit.Assert.*;
 
 /**
  * The Test Class for the Main menu Class (Responsible for starting the game)
@@ -27,12 +29,12 @@ public class MainMenuTest {
     }
 
     /**
-     * The Test is used to check the Getter Method for the Save Button
+     * The Test is used to check the Getter Method for the Start Button
      */
     @Test
     public void testGetStartButton(){
         assertTrue("The Save Menu Item is functional command",testFrame.getStartButton().isEnabled());
-
+        assertEquals("The Image Icon properly placed", new ImageIcon(getClass().getResource("/Images/StartPage.jpg")).toString(),testFrame.getStartButton().getIcon().toString());
     }
 
     @After
